10b29643deb052b5a2257f903c0ff375d661ab14
Merve Ozturk <ozturkmerve745@gmail.com>
2023-11-26 19:12:10 +0300

    cart

---
+++ src/components/cart/CartSummary.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
+class CartSummary extends Component {
+  renderEmpty() {
+    return (
+      <NavItem>
+        <NavLink>Components</NavLink>
+      </NavItem>
+    );
+  }
+  renderSummary() {
+    return (
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle nav caret>
+          Sepetiniz
+        </DropdownToggle>
+        <DropdownMenu end>
+          <DropdownItem>Option 1</DropdownItem>
+          <DropdownItem>Option 2</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Sepete Git</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    );
+  }
+  render() {
+    return (
+      <div>
+        {this.props.cart.length > 0 ? this.renderSummary() : this.renderEmpty()}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    cart: state.cartReducer,
+  };
+}
+
+export default connect(mapStateToProps)(CartSummary);
--- src/components/navi/Navi.js
+++ src/components/navi/Navi.js
@@ -7,12 +7,13 @@ import {
   Nav,
   NavItem,
   NavLink,
+  NavbarText,
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  NavbarText,
 } from "reactstrap";
+import CartSummary from "../cart/CartSummary";
 
 function Example(args) {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,26 +26,16 @@ function Example(args) {
         <NavbarBrand href="/">reactstrap</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
+
           <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
+           
             <NavItem>
               <NavLink href="https://github.com/reactstrap/reactstrap">
                 GitHub
               </NavLink>
             </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu end>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
+    
+            <CartSummary />
           </Nav>
           <NavbarText>Simple Text</NavbarText>
         </Collapse>
